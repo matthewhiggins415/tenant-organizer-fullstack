ebab76576ae765f802d161e9df4e914cf73ea2c0
matthewhiggins415 <mhiggins.louis@gmail.com>
2022-02-21 11:02:47 -1000

    updating user model to hold personal info.

--- app/models/landlord.js
+++ app/models/landlord.js
@@ -2,6 +2,15 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const LandlordSchema = new Schema({
+  firstName: { 
+    type: String
+  }, 
+  lastName: {
+    type: String
+  },
+  phone: {
+    type: String
+  },
   email: {
     type: String,
     required: true,
